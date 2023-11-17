66bf3fb2559cb25f265b97f2c862cf9f532a9ca1
kavishkamk <kavishkamadhushan727@gmail.com>
2023-11-17 19:38:22 +0530

    add: realm roles and groups

---
+++ src/types/realm-roles.ts
@@ -0,0 +1,17 @@
+/**
+ * Realm roals are used to identify the user permissions in the application.
+ * These roal codes are used to identify the user permissions available in the keycloak.
+ * @enum {string}
+ */
+enum RealmRoals {
+    AccountAdmin = "account-admin",
+    ADMIN = "admin",
+    LECTURER = "lecture",
+    STUDENT = "student",
+    USER = "user",
+}
+
+    
+export {
+    RealmRoals
+};
